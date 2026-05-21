a6505d8ff851df30bf33e4a08f52d5d5bc434e87
yush <yus-sato@sakura.ad.jp>
2026-05-21 10:56:25 +0000

    update

---
+++ 9000_ubuntu-24.04-cloudinit_install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -eux
+
+IMAGE_URL=https://ftp.udx.icscoe.jp/Linux/ubuntu-cloud-images/noble/current/noble-server-cloudimg-amd64.img
+IMAGE_DIR=/var/lib/vz/template/iso
+IMAGE_NAME=noble-server-cloudimg-amd64.img
+VM_ID=9000
+VM_NAME=ubuntu-24.04-cloudinit
+BRIDGE=vmbr0
+STORAGE=local-lvm
+CICUSTOM_USER=local:snippets/${VM_ID}_ubuntu-24.04-cloudinit_user.yaml
+CICUSTOM_META=local:snippets/${VM_ID}_ubuntu-24.04-cloudinit_meta.yaml
+
+curl -sLo "$IMAGE_DIR/$IMAGE_NAME" "$IMAGE_URL"
+
+FORMAT=$(qemu-img info "$IMAGE_DIR/$IMAGE_NAME" | awk -F': ' '/file format:/ {print $2}')
+IMAGE_TMP=$IMAGE_DIR/$IMAGE_NAME.tmp
+qemu-img create -f $FORMAT $IMAGE_TMP 20G
+virt-resize --expand /dev/sda1 $IMAGE_DIR/$IMAGE_NAME $IMAGE_TMP
+mv $IMAGE_TMP $IMAGE_DIR/$IMAGE_NAME
+
+qm create $VM_ID \
+	--name $VM_NAME \
+	--memory 512 \
+	--cores 1 \
+	--cpu "x86-64-v4,flags=+nested-virt" \
+	--net0 virtio,bridge="$BRIDGE" \
+	--scsihw virtio-scsi-pci \
+	--agent enabled=1
+
+qm set $VM_ID --scsi0 "local-lvm:0,import-from=$IMAGE_DIR/$IMAGE_NAME"
+
+qm set $VM_ID --ide2 "local-lvm:cloudinit"
+
+qm set $VM_ID --serial0 socket --vga serial0
+
+qm set $VM_ID --boot order=scsi0
+
+qm resize $VM_ID scsi0 20G
+
+virt-customize \
+        --format raw \
+        -a "$(pvesm path $(qm config "$VM_ID" | awk -F': ' '/^scsi0:/ {print $2}' | cut -d',' -f1))" \
+        --install qemu-guest-agent \
+        --run-command 'apt-get clean' \
+        --run-command 'systemctl enable qemu-guest-agent'
+
+qm template $VM_ID
+
+qm set $VM_ID \
+	--cicustom "user=${CICUSTOM_USER},meta=${CICUSTOM_META}" \
+	--ipconfig0 ip=dhcp,ip6=auto \
+	--nameserver "1.1.1.1 1.0.0.1" \
+	--searchdomain "localhost" | sed 's/ -/\n\t-/g'
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # proxmox-cloudinit-template
+
+`/var/lib/vz/snippets`に配置することで利用できます
